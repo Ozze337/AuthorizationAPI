dd25e09b5d38a2f2dd5ce52c1b82098bf7bd10f9
Oskar Buklarewicz <plamacs2@gmail.com>
2025-01-21 15:53:09 +0100

    k

--- AuthorizationAPI/Database/User.cs
+++ AuthorizationAPI/Database/User.cs
@@ -5,8 +5,8 @@ namespace AuthorizationAPI.Database
     public class User : IdentityUser
     {
         public string? Name { get; set; }
-       // public bool IsAdmin { get; set; }
-       // public bool IsStudent { get; set; }
-       // public bool IsTeacher { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsStudent { get; set; }
+        public bool IsTeacher { get; set; }
     }
 }
