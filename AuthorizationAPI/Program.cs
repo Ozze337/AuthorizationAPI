4564ed50f2026cc8decaf049914a0a1a4f8e1ae9
Oskar Buklarewicz <plamacs2@gmail.com>
2025-01-24 19:57:01 +0100

    admin can add roles to users by finding their Id

--- AuthorizationAPI/Program.cs
+++ AuthorizationAPI/Program.cs
@@ -1,118 +1,223 @@
 using AuthorizationAPI.Database;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using System.Security.Claims;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthorization(options =>
+builder.Services.Configure<IdentityOptions>(options =>
 {
-    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-    options.AddPolicy("RequireTeacherRole", policy => policy.RequireRole("Teacher"));
-    options.AddPolicy("RequireStudentRole", policy => policy.RequireRole("Student"));
+    // Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 2;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
 });
 
 
-builder.Services.AddIdentityCore<User>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
 
-
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme)
-    .AddBearerToken(IdentityConstants.BearerScheme)
-    ;
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"))
+    .AddPolicy("RequireTeacherRole", policy => policy.RequireRole("Teacher"))
+    .AddPolicy("RequireStudentRole", policy => policy.RequireRole("Student"));
 
 builder.Services.AddIdentityCore<User>()
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddApiEndpoints();
 
-
-builder.Services.AddDbContext<AppDbContext>(options =>
- options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddEndpointsApiExplorer();
+//add swagger with jwt
+builder.Services.AddSwaggerGen(options =>
+{
+    //add jwt
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            []
+        }
+    });
+});
 
 
 var app = builder.Build();
 
+
+app.MapIdentityApi<User>();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<AppDbContext>();
-    context.Database.Migrate();
+    var dbContext = services.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
 }
-using (var scope = app.Services.CreateScope())
+    using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<AppDbContext>();
+    var dbContext = services.GetRequiredService<AppDbContext>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
     var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+    dbContext.Database.Migrate();
 
-    context.Database.Migrate();
+    //create admin account if there are no users
+    if (!dbContext.Users.Any())
+    {
 
-    if (!await roleManager.RoleExistsAsync("Admin"))
-    {
-        await roleManager.CreateAsync(new IdentityRole("Admin"));
-    }
-    if (!await roleManager.RoleExistsAsync("Teacher"))
-    {
-        await roleManager.CreateAsync(new IdentityRole("Teacher"));
-    }
-    if (!await roleManager.RoleExistsAsync("Student"))
-    {
-        await roleManager.CreateAsync(new IdentityRole("Student"));
+        var admin = new User
+        {
+            UserName = "admin@wp.pl",
+            Email = "admin@wp.pl",
+            EmailConfirmed = true
+        };
+
+        var result = await userManager.CreateAsync(admin, "qwerty");
+
+        //throw exception if failed
+        if (result.Succeeded == false)
+        {
+            throw new Exception("Cannot create admin account");
+        }
+
+        //check if admin role exists
+
+        if (!await roleManager.RoleExistsAsync("Admin"))
+        {
+            await roleManager.CreateAsync(new IdentityRole("Admin"));
+        }
+
+        await userManager.AddToRoleAsync(admin, "Admin");
+
+        //save changes
+        await dbContext.SaveChangesAsync();
     }
 }
 
 app.MapGet("students/me", async (ClaimsPrincipal claims, AppDbContext context) =>
 {
-    string userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+    var userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
     return await context.Students.FindAsync(userId);
 })
-.RequireAuthorization("RequireStudentRole");
+    .RequireAuthorization("RequireStudentRole");
 
 app.MapGet("teachers/me", async (ClaimsPrincipal claims, AppDbContext context) =>
 {
-    string userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+    var userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
     return await context.Teachers.FindAsync(userId);
 })
-.RequireAuthorization("RequireTeacherRole");
+    .RequireAuthorization("RequireTeacherRole");
 
-app.MapGet("admins/me", async (ClaimsPrincipal claims, AppDbContext context) =>
+app.MapGet("admins/me", async (ClaimsPrincipal user, AppDbContext context) =>
 {
-    string userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-    return await context.Admins.FindAsync(userId);
+    var userId = user.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+    var name = user.Identity!.Name!;
+    //get user from db
+    var userFromDb = context.Users.Find(userId);
+    return userFromDb;
+})
+    .RequireAuthorization("RequireAdminRole");
+
+app.MapGet("/me", async (ClaimsPrincipal claims, AppDbContext context) =>
+{
+    var userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+    if(userId == null)
+    {
+        throw new ArgumentNullException(nameof(userId));
+    }
+    var user = await context.Users.FindAsync(userId);
+
+    if (await context.Students.AnyAsync(s => s.Id == userId))
+    {
+        return await context.Students.FindAsync(userId);
+    }
+    else if (await context.Teachers.AnyAsync(t => t.Id == userId))
+    {
+        return await context.Teachers.FindAsync(userId);
+    }
+    else if (await context.Admins.AnyAsync(a => a.Id == userId))
+    {
+        return await context.Admins.FindAsync(userId);
+    }
+    else
+    {
+        return user;
+    }
+});
+app.MapPost("/assign-role", async (string userId, string role, UserManager<User> userManager, RoleManager<IdentityRole> roleManager) =>
+{
+    var user = await userManager.FindByIdAsync(userId);
+    if (user == null)
+    {
+        return Results.NotFound("User not found");
+    }
+
+    if (!await roleManager.RoleExistsAsync(role))
+    {
+        return Results.BadRequest("Role does not exist");
+    }
+
+    var result = await userManager.AddToRoleAsync(user, role);
+    if (result.Succeeded)
+    {
+        return Results.Ok("Role assigned successfully");
+    }
+
+    return Results.BadRequest("Failed to assign role");
 })
 .RequireAuthorization("RequireAdminRole");
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-
 }
+
 app.MapGet("users/me", async (ClaimsPrincipal claims, AppDbContext context) =>
 {
-    string userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+    var userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
     return await context.Users.FindAsync(userId);
 })
-.RequireAuthorization();
-
+    .RequireAuthorization();
+app.MapGet("manage/rolesAndClaims", (ClaimsPrincipal user) => {
+    return Results.Ok(new
+    {
+        Roles = user.FindAll(ClaimTypes.Role).Select(claim => claim.Value),
+        Claims = user.Claims.ToDictionary(claim => claim.Type, claim => claim.Value)
+    });
+}).RequireAuthorization();
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapIdentityApi<User>();
 
 app.Run();
